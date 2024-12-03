7523ce72372cfc15ba592dd4ad95d98b4e8c4979
emmTGB <79254221+emmTGB@users.noreply.github.com>
2024-12-03 16:24:43 +0800

    Initial commit

---
+++ src/main/java/com/oxlxs/hotelmanagementsysback/HotelManagementSysBackApplication.java
@@ -0,0 +1,13 @@
+package com.oxlxs.hotelmanagementsysback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSysBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSysBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oxlxs/hotelmanagementsysback/HotelManagementSysBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxlxs.hotelmanagementsysback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSysBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
